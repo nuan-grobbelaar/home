261c175113afc01d59b99cf28be82d3015e98d60
Nuan Grobbelaar <ngrobbelaar@mezzanineware.com>
2022-05-22 16:54:11 +0200

    gd

---
+++ src/components/Temperature.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import "../App.css"
+import styles from './Temperature.module.css'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function Temperature(props) {
+
+  const getClasses = () => {
+    if (props.size == 'large') return ['large-temp-main', 'large-temp-accent'];
+    if (props.size == 'medium') return ['medium-temp-main', 'medium-temp-accent'];
+    if (props.size == 'small') return ['small-temp-main', 'small-temp-accent'];
+    return ['', ''];
+  }
+
+  let classes = getClasses();
+
+  return (
+    <Box sx={props.sx} className={styles.box}>
+      <Typography className={styles[classes[0]]}>
+        {props.children ? props.children : 'NaN'}
+      </Typography>
+      <Typography className={styles[classes[1]]}>
+        {props.children ? 'c' : ''}
+      </Typography>
+    </Box>
+  );
+}
+
+export default Temperature;
